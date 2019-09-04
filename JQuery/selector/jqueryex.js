249dec5b7fe503791996498ea9e9b0f30a1a9eff
James <jaames.vu@gmail.com>
2019-09-04 15:49:35 -0400

    more jquery

---
+++ JQuery/selector/jqueryex.js
@@ -0,0 +1,10 @@
+if(jQuery){
+    alert("jQuery Loaded!")
+} else{
+    alert("No jQuery :(")
+}
+
+$("div").css("background","purple")
+$(".highlight").css("width", "200px")
+$("#third").css("border","3px orange dashed")
+$("div:first-of-type").css("color","pink")
