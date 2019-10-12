206a33e3c4748d62689776d392c8fed70c470f0f
James <jaames.vu@gmail.com>
2019-10-11 22:46:02 -0400

    Todo Project

---
+++ TodoListProject/assets/js/todos.js
@@ -0,0 +1,14 @@
+// Check off specific todos by clicking
+$("li").click(function(){
+    $(this).toggleClass("completed");
+});
+
+
+//Click on X to delete Todos
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
