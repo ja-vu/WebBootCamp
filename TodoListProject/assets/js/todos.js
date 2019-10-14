1c0ba308c86e27a6dace20482839826e11dd78ba
James <jaames.vu@gmail.com>
2019-10-14 15:58:11 -0400

    todo list project

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,14 +1,30 @@
 // Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("completed");
 });
 
 
 //Click on X to delete Todos
-$("span").click(function(event){
+$("ul").on("click","span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
 });
 
+$("input[type='text']").keypress(function(event){
+    // if we hit enter (character #13)
+    if(event.which === 13){
+
+        //grabbing new todo text from input
+        var toDoText = $(this).val();
+        $(this).val("")
+
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + toDoText + "</li>")
+    }
+});
+
+$(".fa-edit").click(function(){
+    $("input[type='text']").fadeToggle();
+})
